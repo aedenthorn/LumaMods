18847273c3609c153725e2d29d3dab5b968a2a6c
aedenthorn <aedenthorn1@gmail.com>
2025-11-11 20:12:48 +0530

    etc

---
+++ AdvancedSort/AdvancedItemStackComparer.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+
+namespace AdvancedSort
+{
+    internal class AdvancedItemStackComparer : IComparer<ItemStack>
+    {
+        private BepInExPlugin.SortType sortType;
+        private bool ascending;
+
+        private readonly Func<ItemStack, bool> m_isItemInvalid;
+
+        public AdvancedItemStackComparer(Func<ItemStack, bool> isItemInvalid, BepInExPlugin.SortType sortType, bool ascending)
+        {
+            this.sortType = sortType;
+            this.ascending = ascending;
+            m_isItemInvalid = isItemInvalid;
+        }
+
+        public int Compare(ItemStack x, ItemStack y)
+        {
+            if (x == y)
+            {
+                return 0;
+            }
+            if (x?.item == null || x.amount == 0)
+            {
+                return 1;
+            }
+            if (y?.item == null || y.amount == 0)
+            {
+                return -1;
+            }
+
+            if (m_isItemInvalid != null)
+            {
+                int num = (m_isItemInvalid(x) ? 1 : 0);
+                int num2 = (m_isItemInvalid(y) ? 1 : 0);
+                if (num != num2)
+                {
+                    return num.CompareTo(num2);
+                }
+            }
+
+            int result = 0;
+
+            switch (sortType)
+            {
+                case BepInExPlugin.SortType.Name:
+                    result = SortByName(x, y);
+                    if(result == 0)
+                        result = SortByType(x, y);
+                    break;
+                case BepInExPlugin.SortType.Type:
+                    result = SortByType(x, y);
+                    if (result == 0)
+                        result = SortByName(x, y);
+                    break;
+                case BepInExPlugin.SortType.Usage:
+                    result = SortByUsage(x, y);
+                    if (result == 0)
+                        result = SortByName(x, y);
+                    break;
+                case BepInExPlugin.SortType.Value:
+                    result = SortByValue(x, y);
+                    if (result == 0)
+                        result = SortByName(x, y);
+                    break;
+            }
+
+            if (result == 0)
+            {
+                result = -1 * x.amount.CompareTo(y.amount);
+            }
+            return result;
+        }
+
+        private int SortByName(ItemStack x, ItemStack y)
+        {
+            var result = x.item.GetDescriptiveName().CompareTo(y.item.GetDescriptiveName());
+            return result * (ascending ? 1 : -1);
+        }
+
+        private int SortByValue(ItemStack x, ItemStack y)
+        {
+            var result = x.item.type.GetSellValue().CompareTo(y.item.type.GetSellValue());
+            return result * (ascending ? 1 : -1);
+        }
+
+        private int SortByUsage(ItemStack x, ItemStack y)
+        {
+            int result = 0;
+            if (x.item.type == null && y.item.type == null)
+                result = 0;
+            else if (x.item.type == null)
+                result = -1;
+            else if (y.item.type == null)
+                result = 1;
+            else
+            {
+                var xl = new LocalizedString("GUI", "comma-list")
+                {
+                    Arguments = new List<object> { x.item.type.GetLocalizedUses() }
+                };
+                var xs = xl.GetLocalizedString();
+                var yl = new LocalizedString("GUI", "comma-list")
+                {
+                    Arguments = new List<object> { y.item.type.GetLocalizedUses() }
+                };
+                var ys = yl.GetLocalizedString();
+                result = xs.CompareTo(ys);
+                BepInExPlugin.Dbgl($"result: {result}, x: {xs}, y: {ys}");
+            }
+            return result * (ascending ? 1 : -1);
+        }
+
+        private int SortByType(ItemStack x, ItemStack y)
+        {
+            int result = 0;
+            if (x.item.type == null && y.item.type == null)
+                result = 0;
+            else if (x.item.type == null)
+                result = -1;
+            else if (y.item.type == null)
+                result = 1;
+            else
+            {
+                var xs = x.item.type.GetClassificationName();
+                var ys = y.item.type.GetClassificationName();
+
+                result = xs.CompareTo(ys);
+                BepInExPlugin.Dbgl($"result: {result}, x: {xs}, y: {ys}");
+            }
+            return result * (ascending ? 1 : -1);
+        }
+    }
+}
---
+++ AdvancedSort/BepInExPlugin.cs
@@ -0,0 +1,159 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Localization.Components;
+using UnityEngine.UI;
+
+namespace AdvancedSort
+{
+    [BepInPlugin("aedenthorn.AdvancedSort", "Advanced Sort", "0.1.0")]
+    public partial class BepInExPlugin : BaseUnityPlugin
+    {
+        public static ConfigEntry<bool> modEnabled;
+        public static ConfigEntry<bool> isDebug;
+        public static ConfigEntry<bool> sortEmptySlots;
+        public static ConfigEntry<bool> currentSortAsc;
+        public static ConfigEntry<SortType> currentSort;
+
+        public static BepInExPlugin context;
+
+        public static void Dbgl(object str, LogLevel level = LogLevel.Debug)
+        {
+            if (isDebug.Value)
+                context.Logger.Log(level, str);
+        }
+
+        public enum SortType
+        {
+            Name,
+            Type,
+            Usage,
+            Value
+        }
+
+        public void Awake()
+        {
+            context = this;
+            modEnabled = Config.Bind<bool>("General", "Enabled", true, "Enable this mod");
+            isDebug = Config.Bind<bool>("General", "IsDebug", true, "Enable debug log");
+            sortEmptySlots = Config.Bind<bool>("Options", "SortEmptySlots", true, "Sort Empty Slots");
+            
+            currentSort = Config.Bind<SortType>("ZInternal", "CurrentSort", SortType.Type, "Current Sort Type");
+            currentSortAsc = Config.Bind<bool>("ZInternal", "CurrentSortAsc", true, "Current Sort Asc");
+
+            if (!modEnabled.Value)
+                return;
+
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), MetadataHelper.GetMetadata(this).GUID );
+
+        }
+        private static void MakeButtons(ButtonWidget sortButton, bool offset = false)
+        {
+            bool found = false;
+            foreach(Transform c in sortButton.transform.parent)
+            {
+                if (c.name == sortButton.name + "_asc")
+                {
+                    var t = c.GetComponentInChildren<TextMeshProUGUI>();
+                    t.text = currentSortAsc.Value ? "Asc." : "Desc.";
+                    found = true;
+                }
+                else if (c.name == sortButton.name + "_type")
+                {
+                    var t = c.GetComponentInChildren<TextMeshProUGUI>();
+                    t.text = currentSort.Value.ToString();
+                    found = true;
+                }
+            }
+            if (found)
+                return;
+            var fi = AccessTools.Field(typeof(ButtonWidget), "m_button");
+            
+            ButtonWidget sortType = Instantiate(sortButton, sortButton.transform.parent);
+            sortType.name = sortButton.name + "_type";
+            DestroyImmediate(sortType.GetComponentInChildren<LocalizeStringEvent>());
+            DestroyImmediate(sortType.GetComponentInChildren<LocalizedFontEvent>());
+            var tmp = sortType.GetComponentInChildren<TextMeshProUGUI>();
+            tmp.text = currentSort.Value.ToString();
+            ((Button)fi.GetValue(sortType)).onClick.AddListener(delegate
+            {
+                CycleSortType();
+                tmp.text = currentSort.Value.ToString();
+            });
+            if (offset)
+            {
+                var rt = sortType.GetComponent<RectTransform>();
+                rt.anchoredPosition += new Vector2(0, 12.5f);
+            }
+
+            ButtonWidget sortAsc = Instantiate(sortButton, sortButton.transform.parent);
+            sortAsc.name = sortButton.name + "_asc";
+            DestroyImmediate(sortAsc.GetComponentInChildren<LocalizeStringEvent>());
+            DestroyImmediate(sortAsc.GetComponentInChildren<LocalizedFontEvent>());
+            var tmp2 = sortAsc.GetComponentInChildren<TextMeshProUGUI>();
+            tmp2.text = currentSortAsc.Value ? "Asc." : "Desc.";
+            ((Button)fi.GetValue(sortAsc)).onClick.AddListener(delegate
+            {
+                currentSortAsc.Value = !currentSortAsc.Value;
+                tmp2.text = currentSortAsc.Value ? "Asc." : "Desc.";
+            });
+            if (offset)
+            {
+                var rt = sortAsc.GetComponent<RectTransform>();
+                rt.anchoredPosition += new Vector2(50, 12.5f);
+            }
+        }
+        private static void CycleSortType()
+        {
+            var types = Enum.GetValues(typeof(SortType));
+            if (currentSort.Value == (SortType)types.GetValue(types.Length - 1))
+                currentSort.Value = (SortType)types.GetValue(0);
+            else
+            {
+                currentSort.Value += 1;
+            }
+        }
+
+        [HarmonyPatch(typeof(PlayerInventoryPanel), nameof(PlayerInventoryPanel.OnCreate))]
+        public static class PlayerInventoryPanel_OnCreate_Patch
+        {
+
+            public static void Prefix(PlayerInventoryPanel __instance, ButtonWidget ___m_sortButton)
+            {
+                if (!modEnabled.Value)
+                    return;
+                MakeButtons(___m_sortButton);
+            }
+
+        }
+        [HarmonyPatch(typeof(UIChestWindow), "OnOpened")]
+        public static class UIChestWindow_OnOpened_Patch
+        {
+
+            public static void Prefix(UIChestWindow __instance, ButtonWidget ___m_sortChestButton, ButtonWidget ___m_sortInventoryButton)
+            {
+                if (!modEnabled.Value)
+                    return;
+                MakeButtons(___m_sortChestButton, true);
+            }
+        }
+        [HarmonyPatch(typeof(Inventory), nameof(Inventory.Sort))]
+        public static class Inventory_Sort_Patch
+        {
+
+            public static void Prefix(Inventory __instance, ref IComparer<ItemStack> order)
+            {
+                if (modEnabled.Value)
+                {
+                    order = new AdvancedItemStackComparer((Func<ItemStack, bool>)AccessTools.Field(typeof(ItemStackComparer), "m_isItemInvalid").GetValue(order), currentSort.Value, currentSortAsc.Value);
+                }
+            }
+        }
+    }
+}
